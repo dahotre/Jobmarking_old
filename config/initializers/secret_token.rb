c4dc0b2165ccd93f443761346b0687697b663017
dahotre <dahotre.aniket@gmail.com>
2012-11-02 23:22:33 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobmarking::Application.config.secret_token = '3158c26b86d34b7d1276721175e2928fcbeec76af69555c5ec614588e08d5b811bdd2fd351cbeb5a21b5ee89190a9615cd30a9a2f57c48d2ae41db3b98f8c50c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jobmarking::Application.config.session_store :cookie_store, :key => '_Jobmarking_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jobmarking::Application.config.session_store :active_record_store

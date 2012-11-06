b4d59b93991afca59503a006c903849bf022b9d4
dahotre <dahotre.aniket@gmail.com>
2012-11-06 00:49:34 -0700

    Added variable to accessible list

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -2,5 +2,5 @@ class Lookup < ActiveRecord::Base
   validates_uniqueness_of :domain
   validates_presence_of :domain, :title, :description
 
-  attr_accessible :title, :description, :location, :otherDomainHits, :deleted
+  attr_accessible :domain, :title, :description, :location, :otherDomainHits, :deleted
 end
